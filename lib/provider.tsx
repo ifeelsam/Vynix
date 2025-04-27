fd9ead5e6991091f25afae71df09e00369a48838
sam <sanskarsharma9005@gmail.com>
2025-04-27 18:59:18 +0530

    add profile and frontend

---
+++ lib/provider.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { PrivyProvider } from '@privy-io/react-auth';
+
+export default function Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <PrivyProvider
+      appId="cm9xlc3oo01cml30m640hsclh"
+      config={{
+        appearance: {
+          landingHeader: "Vynix",
+          loginMessage: "Log in or Sign up to Vynix"
+        },
+        
+        embeddedWallets: {
+          createOnLogin: 'all-users',
+        },
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  );
+}
